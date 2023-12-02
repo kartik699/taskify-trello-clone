da6982f3257b7d8322f2133016ae9870e86227e3
Kartik Gandhi <gandhikartik73@gmail.com>
2023-12-03 01:05:56 +0530

    complete marketing page nav and footer, add authentication

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,18 +1,17 @@
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
-export const Navbar = () => {
+export const Footer = () => {
     return (
-        <div className="fixed top-0 w-full h-14 px-4 border-b flex items-center shadow-sm bg-white">
+        <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
             <div className="md:max-w-screen-xl mx-auto flex items-center w-full justify-between">
                 <Logo />
                 <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-                    <Button size="sm" variant="outline" asChild>
-                        <Link href="/sign-in">Login</Link>
+                    <Button size="sm" variant="ghost">
+                        Privacy Policy
                     </Button>
-                    <Button size="sm" asChild>
-                        <Link href="/sign-up">Get Taskify for Free</Link>
+                    <Button size="sm" variant="ghost">
+                        Terms of Service
                     </Button>
                 </div>
             </div>
