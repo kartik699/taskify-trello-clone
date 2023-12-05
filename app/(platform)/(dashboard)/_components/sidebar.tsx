1fb950820052dddf38d2ddd3ffc60b06b2feacd3
Kartik Gandhi <gandhikartik73@gmail.com>
2023-12-06 00:47:57 +0530

    start sidebar component

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,3 +1,84 @@
-export default function Sidebar() {
-    return <div>Sidebar</div>;
+"use client";
+
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { Accordion } from "@/components/ui/accordion";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface SidebarProps {
+    storageKey?: string;
+}
+
+export default function Sidebar({
+    storageKey = "t-sidebar-state",
+}: SidebarProps) {
+    const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+        storageKey,
+        {}
+    );
+
+    const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+        useOrganization();
+
+    const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+        userMemberships: { infinite: true },
+    });
+
+    const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+        (acc: string[], key: string) => {
+            if (expanded[key]) {
+                acc.push(key);
+            }
+
+            return acc;
+        },
+        []
+    );
+
+    const onExpand = (id: string) => {
+        setExpanded((curr) => ({
+            ...curr, // keep the current state
+            [id]: !expanded[id], // invert the state of the clicked accordion
+        }));
+    };
+
+    if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading)
+        return (
+            <>
+                <Skeleton />
+            </>
+        );
+
+    return (
+        <>
+            <div className="font-medium text-xs flex items-center mb-1">
+                <span className="pl-4">Workspaces</span>
+                <Button
+                    asChild
+                    type="button"
+                    variant="ghost"
+                    size="icon"
+                    className="ml-auto"
+                >
+                    <Link href="/select-org">
+                        <Plus className="h-4 w-4" />
+                    </Link>
+                </Button>
+            </div>
+            <Accordion
+                type="multiple"
+                defaultValue={defaultAccordionValue}
+                className="space-y-2"
+            >
+                {userMemberships.data.map(({ organization }) => (
+                    <p key={organization.id}>{organization.id}</p>
+                ))}
+            </Accordion>
+        </>
+    );
 }
