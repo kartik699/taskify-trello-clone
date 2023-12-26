933b4745f9f12c1d36b02cc456669c926d05451b
Kartik Gandhi <gandhikartik73@gmail.com>
2023-12-27 01:35:15 +0530

    finish rendering cards and card form and its server actions

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Card } from "@prisma/client";
+
+interface CardItemProps {
+    index: number;
+    data: Card;
+}
+
+export default function CardItem({ index, data }: CardItemProps) {
+    return (
+        <div
+            role="button"
+            className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+        >
+            {data.title}
+        </div>
+    );
+}
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -2,10 +2,12 @@
 
 import { ElementRef, useRef, useState } from "react";
 
+import { cn } from "@/lib/utils";
 import { ListWithCards } from "@/types";
 
 import ListHeader from "./list-header";
 import { CardForm } from "./card-form";
+import CardItem from "./card-item";
 
 interface ListItemProps {
     index: number;
@@ -32,6 +34,16 @@ export default function ListItem({ index, data }: ListItemProps) {
         <li className="shrink-0 h-full w-[272px] select-none">
             <div className="w-full rounded-md shadow-md pb-2 bg-[#f1f2f4]">
                 <ListHeader onAddCard={enableEditing} data={data} />
+                <ol
+                    className={cn(
+                        "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+                        data.cards.length > 0 ? "mt-2" : "mt-0"
+                    )}
+                >
+                    {data.cards.map((card, index) => (
+                        <CardItem key={card.id} index={index} data={card} />
+                    ))}
+                </ol>
                 <CardForm
                     listId={data.id}
                     ref={textareaRef}
