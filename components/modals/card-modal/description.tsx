b36b4594bc09675681aa5549d234c6cc861c7f30
Kartik Gandhi <gandhikartik73@gmail.com>
2023-12-31 01:58:25 +0530

    finish card rename functionality and start card description functionality

---
+++ components/modals/card-modal/description.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { AlignLeft } from "lucide-react";
+import { useState, useRef, ElementRef } from "react";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { CardWithList } from "@/types";
+
+interface DescriptionProps {
+    data: CardWithList;
+}
+
+export default function Description({ data }: DescriptionProps) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    return (
+        <div className="flex items-start gap-x-3 w-full">
+            <AlignLeft className="h-5 w-5 mt-0.5 text-neutral-700" />
+            <div className="w-full">
+                <p className="font-semibold text-neutral-700 mb-2">
+                    Description
+                </p>
+                <div
+                    role="button"
+                    className="min-h-[78px] bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
+                >
+                    {data.description || "Add a more detailed description..."}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+Description.Skeleton = () => {
+    return (
+        <div className="flex items-start gap-x-3 w-full">
+            <Skeleton className="h-6 w-6 bg-neutral-200" />
+            <div className="w-full">
+                <Skeleton className="w-24 h-6 mb-2 bg-neutral-200" />
+                <Skeleton className="w-full h-[78px] bg-neutral-200" />
+            </div>
+        </div>
+    );
+};
