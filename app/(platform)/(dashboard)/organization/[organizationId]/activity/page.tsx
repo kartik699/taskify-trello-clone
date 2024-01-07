fed53f7396c24baa1553b52f8a6a0bc85ea14d38
Kartik Gandhi <gandhikartik73@gmail.com>
2024-01-07 19:37:19 +0530

    activity page deployment fix

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -4,11 +4,14 @@ import { Separator } from "@/components/ui/separator";
 
 import { ActivityList } from "./_components/activity-list";
 import Info from "../_components/info";
+import { checkSubscription } from "@/lib/subscription";
 
-export default function ActivityPage() {
+export default async function ActivityPage() {
+    const isPro = await checkSubscription();
+
     return (
         <div className="w-full">
-            <Info />
+            <Info isPro={isPro} />
             <Separator className="my-2" />
             <Suspense fallback={<ActivityList.Skeleton />}>
                 <ActivityList />
